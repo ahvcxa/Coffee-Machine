1b6cc7c02b2090eb17ef8d955c85ca1ec80e569c
BATUHAN INAN <66177097+ahvcxa@users.noreply.github.com>
2024-11-18 18:19:37 +0300

    Added LoginFrame

---
+++ LoginFrame.java
@@ -0,0 +1,98 @@
+package CoffeeMachine;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.Font;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JPasswordField;
+import java.awt.Color;
+
+public class LoginFrame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JPasswordField passwordField;
+	
+	
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginFrame frame = new LoginFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LoginFrame() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		// this is when click x button open automatically main Frame
+		this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                CoffeeMachine coffeeMachine = new CoffeeMachine();
+                coffeeMachine.setVisible(true);
+            }
+        });
+
+		setBounds(760, 350, 432, 270);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 0, 0));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblUser = new JLabel("USER");
+		lblUser.setForeground(Color.WHITE);
+		lblUser.setFont(new Font("Bitstream Charter", Font.BOLD, 16));
+		lblUser.setBounds(66, 77, 86, 15);
+		contentPane.add(lblUser);
+		
+		JLabel lblPassword = new JLabel("PASSWORD");
+		lblPassword.setForeground(Color.WHITE);
+		lblPassword.setFont(new Font("Bitstream Charter", Font.BOLD, 16));
+		lblPassword.setBounds(66, 128, 106, 15);
+		contentPane.add(lblPassword);
+		
+		JButton btnEnter = new JButton("ENTER");
+		btnEnter.setForeground(Color.WHITE);
+		btnEnter.setBackground(Color.BLACK);
+		btnEnter.setFont(new Font("Bitstream Charter", Font.BOLD, 15));
+		btnEnter.setBounds(173, 183, 131, 25);
+		contentPane.add(btnEnter);
+		
+		textField = new JTextField();
+		textField.setForeground(Color.WHITE);
+		textField.setBackground(Color.BLACK);
+		textField.setBounds(173, 71, 131, 25);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		passwordField = new JPasswordField();
+		passwordField.setForeground(Color.WHITE);
+		passwordField.setBackground(Color.BLACK);
+		passwordField.setBounds(173, 124, 131, 25);
+		contentPane.add(passwordField);
+	}
+}
